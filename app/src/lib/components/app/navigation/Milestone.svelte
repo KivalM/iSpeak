2f91b6340023241dc32c63eac282944254e0d7ef
Kival Mahadew <kivalm@protonmail.com>
2024-07-21 11:31:12 +0000

    chore: Refactor navigation components for improved user experience

--- app/src/lib/components/app/navigation/Milestone.svelte
+++ app/src/lib/components/app/navigation/Milestone.svelte
@@ -3,12 +3,24 @@
 
     export let milestone;
     export let selected = false;
+
+    let focus = () => {
+        selected = true;
+    };
+
+    let unfocus = () => {
+        // wait for a second before unfocusing
+        setTimeout(() => {
+            selected = false;
+        }, 100);
+    };
 </script>
 
 <button
     class="btn rounded-full w-16 h-16 relative"
     class:bg-accent={selected}
-    on:focus={() => (selected = true)}
+    on:focus={focus}
+    on:blur={unfocus}
 >
     <div class:opacity-50={!milestone.completed} class="text-4xl">
         <Icon icon="fxemoji:star" />
@@ -16,7 +28,7 @@
 
     {#if selected}
         <div
-            class="absolute chat chat-start left-0 translate-x-[50px] top-0 translate-y-[-40px] w-80"
+            class=" absolute chat chat-start left-0 translate-x-[50px] top-0 translate-y-[-40px] w-80"
         >
             <div class="chat-bubble">
                 <div class="text-lg">{milestone.title}</div>
