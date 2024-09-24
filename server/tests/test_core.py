c4eb60c699625b1d02aa2858ad3e80943b5fe3bc
Kival Mahadew <kivalm@protonmail.com>
2024-09-24 12:10:45 +0200

    Uncomment Tests

--- server/tests/test_core.py
+++ server/tests/test_core.py
@@ -8,41 +8,41 @@ def get_all_files():
     return os.listdir(data_dir)
 
 
-# def test_feedback_pipeline():
-#     feedback = FeedbackPipeline()
-#     assert feedback.pipeline_name == "automatic-speech-recognition"
-#     assert feedback.model_name == "facebook/wav2vec2-lv-60-espeak-cv-ft"
-#     assert feedback.pipe is not None
-#     assert feedback.transcribe is not None
+def test_feedback_pipeline():
+    feedback = FeedbackPipeline()
+    assert feedback.pipeline_name == "automatic-speech-recognition"
+    assert feedback.model_name == "facebook/wav2vec2-lv-60-espeak-cv-ft"
+    assert feedback.pipe is not None
+    assert feedback.transcribe is not None
 
 
-# def test_transcription():
-#     feedback = FeedbackPipeline()
-#     files = get_all_files()
-#     for file in files:
-#         file_path = f"{data_dir}/{file}"
-#         transcription = feedback.transcribe(file_path)
-#         print(f"Transcription for {file}: {transcription}")
-#         assert transcription is not None
-#         assert len(transcription) > 0
+def test_transcription():
+    feedback = FeedbackPipeline()
+    files = get_all_files()
+    for file in files:
+        file_path = f"{data_dir}/{file}"
+        transcription = feedback.transcribe(file_path)
+        print(f"Transcription for {file}: {transcription}")
+        assert transcription is not None
+        assert len(transcription) > 0
 
 
-# def test_feedback():
-#     feedback = FeedbackPipeline()
-#     files = get_all_files()
-#     for file in files[-1:]:
-#         file_path = f"{data_dir}/{file}"
-#         feedback_data = feedback.generate(
-#             "hello, how are you today", file_path)
-#         print(f"Feedback for {file}: {feedback_data}")
-#         assert feedback_data is not None
-#         assert feedback_data["ipa"] is not None
-#         assert feedback_data["phonemes"] is not None
-#         assert len(feedback_data["ipa"]) > 0
-#         assert len(feedback_data["phonemes"]) > 0
-#         # assert feedback_data["word"] == "hello"
-#         assert feedback_data["ipa"] == feedback.transcribe(file_path)
-#         # assert feedback_data["phonemes"] == feedback.get_phonemes("hello")
+def test_feedback():
+    feedback = FeedbackPipeline()
+    files = get_all_files()
+    for file in files[-1:]:
+        file_path = f"{data_dir}/{file}"
+        feedback_data = feedback.generate(
+            "hello, how are you today", file_path)
+        print(f"Feedback for {file}: {feedback_data}")
+        assert feedback_data is not None
+        assert feedback_data["ipa"] is not None
+        assert feedback_data["phonemes"] is not None
+        assert len(feedback_data["ipa"]) > 0
+        assert len(feedback_data["phonemes"]) > 0
+        # assert feedback_data["word"] == "hello"
+        assert feedback_data["ipa"] == feedback.transcribe(file_path)
+        # assert feedback_data["phonemes"] == feedback.get_phonemes("hello")
 
 
 def test_api():
